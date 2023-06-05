eba1e9dd52633df294f8dfc40da772a137a21e4d
Agung  Perdananto <agung.perdananto@sirclo.com>
2023-06-05 16:14:23 +0700

    no message

--- kecerdasan buatan/c413.py
+++ kecerdasan buatan/c413.py
@@ -1,3 +1,4 @@
+from matplotlib import pyplot as plt
 class VariableFuzzy():
 
     def __init__(self):
@@ -17,7 +18,7 @@ class Speed(VariableFuzzy):
         self.s2 = 60
         self.s3 = 80
         self.s4 = 100
-        pass
+        self.sn = 200
     
     def slow(self, x):
         # 0-s1 = 1
@@ -52,4 +53,31 @@ class Speed(VariableFuzzy):
         else:
             return 0
 
-        
+    def graph(self, x=None):
+        # slow
+        plt.figure(figsize=(20, 10), dpi=80)
+        # 0=>s1 [1]
+        # s1=>s2 [down]
+        # s2=>...[0]
+        x_low = [0, self.s1, self.s2, self.sn]
+        y_low = [1, 1, 0, 0]
+        plt.plot(x_low, y_low, label='low')
+        # steady
+        # 0=>s1 [0]
+        # s1=>s2 [up]
+        # s2=>s3 [1]
+        # s3=>s4 [down]
+        # s4=>...[0]
+        x_steady = [0, self.s1, self.s2, self.s3, self.s4, self.sn]
+        y_steady = [0, 0, 1, 1, 0, 0 ]
+        plt.plot(x_steady, y_steady, label='steady')
+        # fast
+        # 0=>s3 [0]
+        # s3=>s4 [up]
+        # s4=>...[1]
+        x_fast = [0, self.s3, self.s4, self.sn]
+        y_fast = [0, 0, 1, 1 ]
+        plt.plot(x_fast, y_fast, label='fast')
+
+        plt.legend(loc="upper left")
+        plt.show()
