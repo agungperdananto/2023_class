a0cf366535873c7534881b1131461e126da80373
Agung  Perdananto <agung.perdananto@sirclo.com>
2023-06-21 09:44:48 +0700

    add fuzzy inference example

--- kecerdasan buatan/fis.py
+++ kecerdasan buatan/fis.py
@@ -9,11 +9,6 @@ class BaseFuzzy():
         return (x - self.minimum )/ (self.maximum - self.minimum)
     def down(self, x):
         return (self.maximum - x)/ (self.maximum - self.minimum)
-# def down(x, xmin, xmax):
-#     return (xmax - x) / (xmax - xmin)
-
-# def up(x, xmin, xmax):
-#     return (x - xmin) / (xmax - xmin)
 
 class Permintaan(BaseFuzzy):
 
@@ -49,8 +44,20 @@ class Permintaan(BaseFuzzy):
         # naik
         y_naik = [0, 0, 1, 1]
         ax.plot(x, y_naik, label='naik')
+        ax.set_title('Permintaan')
+        ax.legend(loc='upper left')
+        if value:
+            # turun
+            x_param = [0, value, value]
+            turun_value = self.turun(value)
+            y_param_turun = [turun_value, turun_value, 0]
+            ax.plot(x_param, y_param_turun, color='C0')
+            # naik
+            naik_value = self.naik(value)
+            y_param_naik = [naik_value, naik_value, 0]
+            ax.plot(x_param, y_param_naik, color='C1')
 
-class Persediaan():
+class Persediaan(BaseFuzzy):
 
     def __init__(self):
         self.p1 = 100
@@ -81,13 +88,26 @@ class Persediaan():
         # sedikit
         x = [0, self.p1, self.p2, self.pn]
         y_sdk = [1, 1, 0, 0]
-        ax.plot(x, y_sdk, label='sedikit')
+        ax.plot(x, y_sdk, label='sedikit', color='C0')
         # banyak
         y_byk = [0, 0, 1, 1]
-        ax.plot(x, y_byk, label='banyak')
+        ax.plot(x, y_byk, label='banyak', color='C1')
+        ax.legend(loc='upper left')
+        ax.set_title('Persediaan')
+        if value:
+            # sedikit
+            x_param = [0, value, value]
+            sedikit_value = self.sedikit(value)
+            y_param_kurang = [sedikit_value, sedikit_value, 0]
+            ax.plot(x_param, y_param_kurang, color='C0')
+            # banyak
+            banyak_value = self.banyak(value)
+            y_param_banyak = [banyak_value, banyak_value, 0]
+            ax.plot(x_param, y_param_banyak, color='C1')
 
 
-class Produksi():
+# output
+class Produksi(BaseFuzzy):
     minimum = 1000
     maximum = 5000
     permintaan = 0
@@ -99,12 +119,43 @@ class Produksi():
         self.pn = 6000
         self.permintaan = 0
         self.persediaan = 0
+        self.kurang = 0
+        self.tambah = 0
+        self.real_value = 0
 
     def _berkurang(self, a):
-        return self.p2 - a*(self.p2 - self.p1)
+        self.kurang = self.p2 - a*(self.p2 - self.p1)
+        return self.kurang
+
 
     def _bertambah(self, a):
-        return a*(self.p2 - self.p1) + self.p1
+        self.tambah =  a*(self.p2 - self.p1) + self.p1
+        return self.tambah
+    
+    @property
+    def fuzzy_berkurang(self):
+        x = self.real_value
+        if x >= self.p2:
+            return 0
+        elif x<= self.p1:
+            return 1
+        else:
+            self.minimum = self.p1
+            self.maximum = self.p2
+            return self.down(x)
+    
+    @property
+    def fuzzy_bertambah(self):
+        x = self.real_value
+        if x >= self.p2:
+            return 1
+        elif x<= self.p1:
+            return 0
+        else:
+            self.minimum = self.p1
+            self.maximum = self.p2
+            return self.up(x)
+    
 
     def _inferensi(self, pmt=Permintaan(), psd=Persediaan()):
         result = []
@@ -140,24 +191,50 @@ class Produksi():
             # data[1] = z
             res_a_z += data[0] * data[1]
             res_a += data[0]
-        return res_a_z/res_a
+        self.real_value = res_a_z/res_a
+        return self.real_value
     
     def graph(self):
-        fig, (ax1, ax2, ax3) = plt.subplots(3, 1)
+        fig, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2)
         pmt = Permintaan()
         psd = Persediaan()
 
-        pmt.graph(ax1)
-        psd.graph(ax2)
-         # sedikit
+        pmt.graph(ax1, self.permintaan)
+        psd.graph(ax2, self.persediaan)
+        # berkurang
         x = [0, self.p1, self.p2, self.pn]
-        y_sdk = [1, 1, 0, 0]
-        ax3.plot(x, y_sdk, label='berkurang')
-        # banyak
-        y_byk = [0, 0, 1, 1]
-        ax3.plot(x, y_byk, label='bertambah')
+        y_krg = [1, 1, 0, 0]
+        ax3.plot(x, y_krg, label='berkurang', color='C0')
+        # bertambah
+        y_tmb = [0, 0, 1, 1]
+        ax3.plot(x, y_tmb, label='bertambah', color='C1')
+        ax3.set_title('Produksi [Output]')
+        ax3.legend(loc='upper left')
+        if self.real_value:
+            value = self.real_value
+            # kurang
+            x_param = [0, value, value]
+            kurang_value = self.fuzzy_berkurang
+            y_param_kurang = [kurang_value, kurang_value, 0]
+            ax3.plot(x_param, y_param_kurang, color='C0')
+            # tambah
+            tambah_value = self.fuzzy_bertambah
+            y_param_tambah = [tambah_value, tambah_value, 0]
+            ax3.plot(x_param, y_param_tambah, color='C1')
 
         plt.show()
 
+
+permintaan = input('Permintaan:')
+persediaan = input('Persediaan:')
 produksi = Produksi()
+
+
+
+produksi.permintaan = float(permintaan) # 2500
+produksi.persediaan = float(persediaan) # 160
+hasil_produksi = produksi.defuzifikasi()
+
+print(f'Hasil produksi {hasil_produksi}')
+
 produksi.graph()
